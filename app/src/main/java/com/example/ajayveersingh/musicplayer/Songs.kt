564f2977018b9faf9e105f81342772b92962df26
Ajayveer Singh <avsr1997@gmail.com>
2018-06-11 00:49:57 +0530

    Main Screen Fragment XML Complete

---
+++ app/src/main/java/com/example/ajayveersingh/musicplayer/Songs.kt
@@ -0,0 +1,6 @@
+package com.example.ajayveersingh.musicplayer
+
+class Songs(var songID: Long,var songTitle:String,var artist:String,var songData:String, var dataAdded:Long)
+{
+
+}
--- app/src/main/java/com/example/ajayveersingh/musicplayer/mainscreen_fragment.kt
+++ app/src/main/java/com/example/ajayveersingh/musicplayer/mainscreen_fragment.kt
@@ -1,29 +1,51 @@
 package com.example.ajayveersingh.musicplayer
 
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.RelativeLayout
+import android.widget.TextView
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class mainscreen_fragment : Fragment() {
 
+    var nowPlayingBottomBar:RelativeLayout?=null
+    var playPauseButton:ImageButton?=null
+    var songTitle:TextView?=null
+    var visibleLayout:RelativeLayout?=null
+    var noSongs:RelativeLayout?=null
+    var recyclerView:RecyclerView?=null
+    var myactivity:Activity?=null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mainscreen_fragment, container, false)
+                              savedInstanceState: Bundle?): View?
+    {
+        val view=inflater!!.inflate(R.layout.fragment_mainscreen_fragment,container,false)
+        visibleLayout=view?.findViewById(R.id.main_content)
+        noSongs=view?.findViewById(R.id.no_songs)
+        nowPlayingBottomBar=view?.findViewById(R.id.bottom_bar)
+        songTitle=view?.findViewById(R.id.current_song)
+        playPauseButton=view?.findViewById(R.id.play_pause_button)
+        recyclerView=view?.findViewById(R.id.all_songs_view)
+
+        return view
     }
 
+    override fun onAttach(context: Context?)
+    {
+        super.onAttach(context)
+        myactivity=context as Activity
+    }
 
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+
+        myactivity=activity
+    }
 }
