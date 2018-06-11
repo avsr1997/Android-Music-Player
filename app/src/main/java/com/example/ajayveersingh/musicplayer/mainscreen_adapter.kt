2a6a62f024b855ddd1a0284c63ed1b1cd19d240d
Ajayveer Singh <avsr1997@gmail.com>
2018-06-11 14:25:46 +0530

    Main Screen Fragment Complete

---
+++ app/src/main/java/com/example/ajayveersingh/musicplayer/mainscreen_adapter.kt
@@ -0,0 +1,63 @@
+package com.example.ajayveersingh.musicplayer
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class mainscreen_adapter(_songDetails:ArrayList<Songs>,_context:Context):RecyclerView.Adapter<mainscreen_adapter.MyViewHolder>()
+{
+    var songDetails:ArrayList<Songs>?=null
+    var mcontext:Context?=null
+    init {
+        this.songDetails=_songDetails
+        this.mcontext=_context
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+       val itemview=LayoutInflater.from(parent.context).inflate(R.layout.main_screen_recyclerview,parent,false)
+        var returnthis=MyViewHolder(itemview)
+        return returnthis
+
+
+    }
+    override fun getItemCount(): Int {
+        if(songDetails==null)
+        {
+            return 0
+        }
+        else
+        {
+            return (songDetails as ArrayList<Songs>).size
+        }
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int)
+    {
+        var songObject=songDetails?.get(position)
+        holder.track_title?.text=songObject?.songTitle
+        holder.track_artist?.text=songObject?.artist
+        holder.contentHolder?.setOnClickListener({
+            Toast.makeText(mcontext,songObject?.songTitle,Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+    {
+        var track_title:TextView?=null
+        var track_artist:TextView?=null
+        var contentHolder:RelativeLayout?=null
+
+        init {
+            track_title=itemView?.findViewById(R.id.track_title)
+            track_artist=itemView?.findViewById(R.id.track_artist)
+            contentHolder=itemView?.findViewById(R.id.contentholder)
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/ajayveersingh/musicplayer/mainscreen_fragment.kt
+++ app/src/main/java/com/example/ajayveersingh/musicplayer/mainscreen_fragment.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -16,60 +18,64 @@ import android.widget.TextView
 
 class mainscreen_fragment : Fragment() {
 
-    var nowPlayingBottomBar:RelativeLayout?=null
-    var playPauseButton:ImageButton?=null
-    var songTitle:TextView?=null
-    var visibleLayout:RelativeLayout?=null
-    var noSongs:RelativeLayout?=null
-    var recyclerView:RecyclerView?=null
-    var myactivity:Activity?=null
-
+    var nowPlayingBottomBar: RelativeLayout? = null
+    var playPauseButton: ImageButton? = null
+    var songTitle: TextView? = null
+    var visibleLayout: RelativeLayout? = null
+    var noSongs: RelativeLayout? = null
+    var recyclerView: RecyclerView? = null
+    var myactivity: Activity? = null
+    var mainscreenadapter: mainscreen_adapter? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View?
-    {
-        val view=inflater!!.inflate(R.layout.fragment_mainscreen_fragment,container,false)
-        visibleLayout=view?.findViewById(R.id.main_content)
-        noSongs=view?.findViewById(R.id.no_songs)
-        nowPlayingBottomBar=view?.findViewById(R.id.bottom_bar)
-        songTitle=view?.findViewById(R.id.current_song)
-        playPauseButton=view?.findViewById(R.id.play_pause_button)
-        recyclerView=view?.findViewById(R.id.all_songs_view)
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.fragment_mainscreen_fragment, container, false)
+        visibleLayout = view?.findViewById(R.id.main_content)
+        noSongs = view?.findViewById(R.id.no_songs)
+        nowPlayingBottomBar = view?.findViewById(R.id.bottom_bar)
+        songTitle = view?.findViewById(R.id.current_song)
+        playPauseButton = view?.findViewById(R.id.play_pause_button)
+        recyclerView = view?.findViewById(R.id.all_songs_view)
 
         return view
     }
 
-    override fun onAttach(context: Context?)
-    {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        var songlist: ArrayList<Songs> = getSongsfromPhone()
+        mainscreenadapter = mainscreen_adapter(songlist as ArrayList<Songs>, myactivity as Context)
+        recyclerView?.layoutManager = LinearLayoutManager(myactivity)
+        recyclerView?.itemAnimator = DefaultItemAnimator()
+        recyclerView?.adapter = mainscreenadapter
+    }
+
+    override fun onAttach(context: Context?) {
         super.onAttach(context)
-        myactivity=context as Activity
+        myactivity = context as Activity
     }
 
     override fun onAttach(activity: Activity?) {
         super.onAttach(activity)
-        myactivity=activity
+        myactivity = activity
     }
 
-    fun getSongsfromPhone():ArrayList<Songs>
-    {
-        var arraylist:ArrayList<Songs> = arrayListOf()
-        var contentresolver=myactivity?.contentResolver
-        var songUri=MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        var cursorSong=contentresolver?.query(songUri,null,null,null,null)
-        if(cursorSong!=null && cursorSong.moveToFirst())
-        {
-            val songID=cursorSong.getColumnIndex(MediaStore.Audio.Media._ID)
-            val songTitle=cursorSong.getColumnIndex(MediaStore.Audio.Media.TITLE)
-            val songArtist=cursorSong.getColumnIndex(MediaStore.Audio.Media.ARTIST)
-            val songData=cursorSong.getColumnIndex(MediaStore.Audio.Media.DATA)
-            val songDate=cursorSong.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED)
-            while(cursorSong.moveToNext())
-            {
-                var currentId=cursorSong.getLong(songID)
-                var currentTitle=cursorSong.getString(songTitle)
-                var currentArtist=cursorSong.getString(songArtist)
-                var currentData=cursorSong.getString(songData)
-                var currentDate=cursorSong.getLong(songDate)
-                arraylist.add(Songs(currentId,currentTitle,currentArtist,currentData,currentDate))
+    fun getSongsfromPhone(): ArrayList<Songs> {
+        var arraylist: ArrayList<Songs> = arrayListOf()
+        var contentresolver = myactivity?.contentResolver
+        var songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        var cursorSong = contentresolver?.query(songUri, null, null, null, null)
+        if (cursorSong != null && cursorSong.moveToFirst()) {
+            val songID = cursorSong.getColumnIndex(MediaStore.Audio.Media._ID)
+            val songTitle = cursorSong.getColumnIndex(MediaStore.Audio.Media.TITLE)
+            val songArtist = cursorSong.getColumnIndex(MediaStore.Audio.Media.ARTIST)
+            val songData = cursorSong.getColumnIndex(MediaStore.Audio.Media.DATA)
+            val songDate = cursorSong.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED)
+            while (cursorSong.moveToNext()) {
+                var currentId = cursorSong.getLong(songID)
+                var currentTitle = cursorSong.getString(songTitle)
+                var currentArtist = cursorSong.getString(songArtist)
+                var currentData = cursorSong.getString(songData)
+                var currentDate = cursorSong.getLong(songDate)
+                arraylist.add(Songs(currentId, currentTitle, currentArtist, currentData, currentDate))
             }
         }
         return arraylist
