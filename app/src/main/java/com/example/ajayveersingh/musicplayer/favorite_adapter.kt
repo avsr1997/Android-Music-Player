ee5e03dacdb0c44da60d3ad18462eeb005d9f5d4
Ajayveer Singh <avsr1997@gmail.com>
2018-06-14 20:17:48 +0530

    Fragments Setup

---
+++ app/src/main/java/com/example/ajayveersingh/musicplayer/favorite_adapter.kt
@@ -0,0 +1,69 @@
+package com.example.ajayveersingh.musicplayer
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+
+class favoriteadapter(_songDetails: ArrayList<Songs>, _context: Context) : RecyclerView.Adapter<favoriteadapter.MyViewHolder>() {
+    var songDetails: ArrayList<Songs>? = null
+    var mcontext: Context? = null
+
+    init {
+        this.songDetails = _songDetails
+        this.mcontext = _context
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemview = LayoutInflater.from(parent.context).inflate(R.layout.main_screen_recyclerview, parent, false)
+        var returnthis = MyViewHolder(itemview)
+        return returnthis
+
+
+    }
+
+    override fun getItemCount(): Int {
+        if (songDetails == null) {
+            return 0
+        } else {
+            return (songDetails as ArrayList<Songs>).size
+        }
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        var songObject = songDetails?.get(position)
+        holder.track_title?.text = songObject?.songTitle
+        holder.track_artist?.text = songObject?.artist
+        var args = Bundle()
+        args.putString("songArtist", songObject?.artist)
+        args.putString("path", songObject?.songData)
+        args.putString("songTitle", songObject?.songTitle)
+        args.putInt("songId", songObject?.songID?.toInt() as Int)
+        args.putInt("songPosition", position)
+        args.putParcelableArrayList("songData", songDetails)
+
+        holder.contentHolder?.setOnClickListener({
+            val songplayingfragment = songplaying_fragment()
+            songplayingfragment.arguments = args
+            (mcontext as FragmentActivity).supportFragmentManager.beginTransaction().replace(R.id.fragment, songplayingfragment).commit()
+
+        })
+    }
+
+    class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        var track_title: TextView? = null
+        var track_artist: TextView? = null
+        var contentHolder: RelativeLayout? = null
+
+        init {
+            track_title = itemView?.findViewById(R.id.track_title)
+            track_artist = itemView?.findViewById(R.id.track_artist)
+            contentHolder = itemView?.findViewById(R.id.contentholder)
+        }
+    }
+}
--- app/src/main/java/com/example/ajayveersingh/musicplayer/favrouites_fragment.kt
+++ app/src/main/java/com/example/ajayveersingh/musicplayer/favrouites_fragment.kt
@@ -1,29 +1,98 @@
 package com.example.ajayveersingh.musicplayer
 
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.RelativeLayout
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_mainscreen_fragment.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class favrouites_fragment : Fragment() {
+
+    var myactivity: Activity? = null
+    var getsongslist: ArrayList<Songs>? = null
+    var nofavorites: TextView? = null
+    var bottombar: RelativeLayout? = null
+    var playpausebutton: ImageButton? = null
+    var songTitle: TextView? = null
+    var recyclerView: RecyclerView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favrouites_fragment, container, false)
+        val view = inflater.inflate(R.layout.fragment_favrouites_fragment, container, false)
+        nofavorites = view?.findViewById(R.id.nofavorites)
+        bottombar = view?.findViewById(R.id.bottom_bar)
+        songTitle = view?.findViewById(R.id.songTitle)
+        playpausebutton = view?.findViewById(R.id.play_pause_button)
+        recyclerView = view?.findViewById(R.id.recycler_id)
+        return view
     }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        myactivity = context as Activity
+    }
 
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+        myactivity = activity
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        getsongslist = getSongsfromPhone()
+        if (getsongslist == null) {
+            recyclerView?.visibility = View.INVISIBLE
+            nofavorites?.visibility = View.VISIBLE
+        } else {
+            var favoritsadapter: favoriteadapter = favoriteadapter(getsongslist as ArrayList<Songs>, myactivity as Context)
+            recyclerView?.layoutManager = LinearLayoutManager(myactivity)
+            recyclerView?.itemAnimator = DefaultItemAnimator()
+            recyclerView?.adapter = favoritsadapter
+            recyclerView?.hasFixedSize()
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?) {
+        super.onPrepareOptionsMenu(menu)
+    }
+
+    fun getSongsfromPhone(): ArrayList<Songs> {
+        var arraylist: ArrayList<Songs> = arrayListOf()
+        var contentresolver = myactivity?.contentResolver
+        var songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        var cursorSong = contentresolver?.query(songUri, null, null, null, null)
+        if (cursorSong != null && cursorSong.moveToFirst()) {
+            val songID = cursorSong.getColumnIndex(MediaStore.Audio.Media._ID)
+            val songTitle = cursorSong.getColumnIndex(MediaStore.Audio.Media.TITLE)
+            val songArtist = cursorSong.getColumnIndex(MediaStore.Audio.Media.ARTIST)
+            val songData = cursorSong.getColumnIndex(MediaStore.Audio.Media.DATA)
+            val songDate = cursorSong.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED)
+            while (cursorSong.moveToNext()) {
+                var currentId = cursorSong.getLong(songID)
+                var currentTitle = cursorSong.getString(songTitle)
+                var currentArtist = cursorSong.getString(songArtist)
+                var currentData = cursorSong.getString(songData)
+                var currentDate = cursorSong.getLong(songDate)
+                arraylist.add(Songs(currentId, currentTitle, currentArtist, currentData, currentDate))
+            }
+        }
+        return arraylist
+    }
 }
